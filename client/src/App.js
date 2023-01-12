e4882f9aa206f1672381272127d7fefcad99d25e
An Nguyen <annguyen01122000@gmail.com>
2023-01-12 00:37:04 -0600

    search box search bar not finish

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { GerneContainer } from './components';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Home />
       <SearchBox />
       <GerneContainer />
