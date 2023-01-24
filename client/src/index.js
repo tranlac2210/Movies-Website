a0e795e1473697a2da5c4985ee0061ecad4306cf
An Nguyen <annguyen01122000@gmail.com>
2023-01-23 23:37:53 -0600

    practice useReducer,useContext

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { SearchboxProvider } from "./context/search_keyword_context";
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchboxProvider>
+      <App />
+    </SearchboxProvider>
   </React.StrictMode>
 );
 
